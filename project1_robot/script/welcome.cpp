bfc36d5a9acb67912b025c38782ba0950fd1e526
Pavan <pavankris@gmail.com>
2019-02-06 12:43:35 +0000

    Added world

--- project1_robot/script/welcome.cpp
+++ project1_robot/script/welcome.cpp
@@ -6,7 +6,7 @@ namespace gazebo
   {
     public: WorldPluginMyRobot() : WorldPlugin()
             {
-              printf("Hello World!\n");
+              printf("Welcome to Pavan's World!\n");
             }
 
     public: void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf)
